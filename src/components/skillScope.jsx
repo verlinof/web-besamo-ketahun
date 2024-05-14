42883ba75b5185b835defb5bc4a1987f971199b4
verlinof <ajikoko.zx@gmail.com>
2024-05-14 10:12:15 +0700

    Fix; SkillScope.jsx

--- src/components/skillScope.jsx
+++ src/components/skillScope.jsx
@@ -6,7 +6,7 @@ const SkillScope = () => {
       <div className="w-full overflow-hidden max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 my-[60px] text-white">
         <p className="text-gray-400 mb-5">What do i do?</p>
         <p className="text-[20px] md:text-[30px] lg:text-[38px] w-full lg:w-[811px]">
-          These are some skills I've picked up over my career.
+          These are some skills I`ve picked up over my career.
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-5 my-[50px]">
           <CardSkillScope
