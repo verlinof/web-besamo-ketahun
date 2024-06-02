2b3ee97a53496271926a0c2f0f4dd832f1e7df93
verlinof <ajikoko.zx@gmail.com>
2024-06-02 23:13:43 +0700

    Fix: Process Page

--- src/components/workStep.jsx
+++ src/components/workStep.jsx
@@ -1,4 +1,4 @@
-const WorkStep = ({ step, title, description1, description2 }) => {
+const WorkStep = ({ step, title, description1, children }) => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-5 my-[50px]">
       <div className="grid grid-cols-1">
@@ -7,7 +7,7 @@ const WorkStep = ({ step, title, description1, description2 }) => {
       </div>
       <div className="grid grid-cols-1 my-2 lg:my-0 col-span-2">
         <p className="text-[18px] text-secondary-gray">{description1}</p>
-        <p className="text-[18px] text-secondary-gray my-3">{description2}</p>
+        <p className="text-[18px] text-secondary-gray my-3">{children}</p>
         <hr className="border border-gray-700 mt-[40px]" />
       </div>
     </div>
