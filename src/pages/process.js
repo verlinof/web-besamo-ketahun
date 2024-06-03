709eb10e1b8460e47e85153a49ef037276b82c71
verlinof <ajikoko.zx@gmail.com>
2024-06-03 18:52:18 +0700

    Final Push

--- src/pages/process.js
+++ src/pages/process.js
@@ -14,37 +14,132 @@ export default function Process() {
           <WorkStep
             step={"Step 01"}
             title={"Get Started"}
-            description1={"Starting with meeting to discuss about your needs."} >
+            description1={"Starting with meeting to discuss about your needs."}
+          >
             <p>
-              <span className="text-white">Why?</span>{" "}Because understanding your needs and requirements is crucial, thoroughly addressing these aspects ensures that the project`s objectives are aligned with the
-              your expectations. This alignment not only enhances stakeholder satisfaction but also fosters trust and cooperation, ultimately contributing to the project`s success.
-              Additionally, it helps in identifying potential risks early, enabling proactive management and increasing the likelihood of project success.
+              <span className="text-white">Why?</span> Because understanding
+              your needs and requirements is crucial, thoroughly addressing
+              these aspects ensures that the project`s objectives are aligned
+              with the your expectations. This alignment not only enhances
+              stakeholder satisfaction but also fosters trust and cooperation,
+              ultimately contributing to the project`s success. Additionally, it
+              helps in identifying potential risks early, enabling proactive
+              management and increasing the likelihood of project success.
             </p>
           </WorkStep>
           <WorkStep
             step={"Step 02"}
             title={"Planning"}
-            description1={"Defining clear goals and good planning."} >
+            description1={"Defining clear goals and good planning."}
+          >
             <p>
-              <span className="text-white">At begins,</span> {" "} defining clear goals is essential: Establishing well-defined goals ensures that all team members understand the project`s purpose and objectives, leading
-              to a focused and cohesive effort. This clarity not only enhances motivation and alignment but also provides measurable benchmarks for success, driving the project towards its desired outcomes.
+              <span className="text-white">At begins,</span> defining clear
+              goals is essential: Establishing well-defined goals ensures that
+              all team members understand the project`s purpose and objectives,
+              leading to a focused and cohesive effort. This clarity not only
+              enhances motivation and alignment but also provides measurable
+              benchmarks for success, driving the project towards its desired
+              outcomes.
             </p>
             <br />
             <p>
-              <span className="text-white">Choosing the right tech stack is critical,</span> {" "} Selecting the appropriate technology stack ensures that the project is built on robust, scalable, and
-              maintainable foundations. This decision influences the app`s performance, development speed, and future scalability, directly impacting the overall success and sustainability of the project.
+              <span className="text-white">
+                Choosing the right tech stack is critical,
+              </span>{" "}
+              Selecting the appropriate technology stack ensures that the
+              project is built on robust, scalable, and maintainable
+              foundations. This decision influences the app`s performance,
+              development speed, and future scalability, directly impacting the
+              overall success and sustainability of the project.
             </p>
             <br />
             <p>
-              <span className="text-white">Understanding the app flow is fundamental, </span> {" "}
-              A well-designed app flow guarantees a smooth and intuitive user experience, reducing friction and increasing user satisfaction. By
-              mapping out the user journey, potential issues can be identified and addressed early, leading to a more polished and user-friendly application.
+              <span className="text-white">
+                Understanding the app flow is fundamental,{" "}
+              </span>{" "}
+              A well-designed app flow guarantees a smooth and intuitive user
+              experience, reducing friction and increasing user satisfaction. By
+              mapping out the user journey, potential issues can be identified
+              and addressed early, leading to a more polished and user-friendly
+              application.
             </p>
             <br />
             <p>
-              <span className="text-white">Good requirement analysis is vital, </span> {" "}
-              Conducting comprehensive requirement analysis ensures that the project meets both functional and non-functional needs. This process
-              aligns the development with stakeholder expectations, mitigates risks, and sets clear criteria for success, ensuring the project delivers value and meets its goals efficiently.
+              <span className="text-white">
+                Good requirement analysis is vital,{" "}
+              </span>{" "}
+              Conducting comprehensive requirement analysis ensures that the
+              project meets both functional and non-functional needs. This
+              process aligns the development with stakeholder expectations,
+              mitigates risks, and sets clear criteria for success, ensuring the
+              project delivers value and meets its goals efficiently.
+            </p>
+            <br />
+          </WorkStep>
+          <WorkStep
+            step={"Step 03"}
+            title={"Design"}
+            description1={"Defining diagrams and good design."}
+          >
+            <p>
+              <span className="text-white">
+                By creating good diagrams and design,
+              </span>{" "}
+              you ensure that the project's requirements are thoroughly
+              understood and closely aligned with the technical specifications.
+              This clarity not only enhances team motivation and alignment by
+              providing a shared vision, but it also sets measurable benchmarks
+              for success, guiding the project efficiently towards its desired
+              outcomes. Effective diagrams and design practices facilitate
+              seamless communication and collaboration among team members,
+              enabling them to visualize and comprehend the app flow, system
+              architecture, and design elements. This comprehensive
+              understanding ensures that every aspect of the project is
+              meticulously planned and executed, ultimately contributing to the
+              overall success and quality of the final product. By maintaining a
+              focus on detailed and precise design, the project can adapt to
+              changes more readily and meet its goals more consistently.
+            </p>
+            <br />
+          </WorkStep>
+          <WorkStep
+            step={"Step 04"}
+            title={"Development"}
+            description1={`Make your expectation come true.`}
+          >
+            <p>
+              <span className="text-white">
+                We will deliver a Good Quality Software for you,
+              </span>{" "}
+              We will leverage the most suitable and cutting-edge technologies
+              to build your project. By selecting the right tools and platforms,
+              we ensure that your project is developed with optimal efficiency,
+              scalability, and performance. Our approach guarantees that the
+              technological foundation of your project is robust and capable of
+              meeting all your current and future requirements. This commitment
+              to using the best available technologies will drive your project
+              towards success, delivering a high-quality product that stands out
+              in the market.
+            </p>
+            <br />
+          </WorkStep>
+          <WorkStep
+            step={"Step 05"}
+            title={"Launch"}
+            description1={`Delivering the project to you.`}
+          >
+            <p>
+              <span className="text-white">After finish the project,</span> We
+              will conduct thorough testing on your project to ensure its
+              reliability, functionality, and performance. Our comprehensive
+              testing process involves rigorous examination of every aspect of
+              the project, including functionality, usability, security, and
+              compatibility. By rigorously testing your project before
+              deployment, we identify and address any potential issues or bugs,
+              ensuring a smooth and successful launch. This commitment to
+              quality assurance guarantees that your project meets the highest
+              standards and delivers an exceptional experience to your users
+              from day one.
             </p>
             <br />
           </WorkStep>
